f1f104ae98a5df94ab65a36aeccf2b3e3d2aad4b
Nugzars Lortkipanidze <numashem@gmail.com>
2019-07-30 10:05:28 +0300

    commit

---
+++ scripts.js
@@ -0,0 +1,84 @@
+console.log('Homework 3');
+
+var myslider = document.getElementById('myslider');
+var myoutput = document.getElementById('myoutput');
+myoutput.innerHTML = myslider.value; // Display the default slider value
+
+// update the current slider value (each time you drag the slider handle)
+myslider.oninput = function () {
+    myoutput.innerHTML = this.value;
+}
+
+var mymain = document.getElementById('mymain');
+
+// clear main
+function clearDivs() {
+    mymain.innerHTML = null;
+}
+
+// generate divs on click in main
+function generateDivs() {
+    // clear main before doing anything
+    clearDivs();
+
+    var mysliderValue = myslider.value;
+    var myFizzValue = document.getElementById('myfizz').value;
+    var myBuzzValue = document.getElementById('mybuzz').value;
+
+    var mydivs = [];
+    for (i = 1; i <= mysliderValue; i++) {
+        var mydiv = document.createElement('div');
+
+        // set id
+        mydiv.id = 'myid' + i;
+
+        // set color and text
+        if (i % myFizzValue === 0 && i % myBuzzValue === 0) {
+            mydiv.className = 'coral';
+            mydiv.innerText = 'FizzBuzz';
+        } else if (i % myFizzValue === 0) {
+            mydiv.className = 'violet';
+            mydiv.innerText = 'Fizz';
+        } else if (i % myBuzzValue === 0) {
+            mydiv.className = 'green';
+            mydiv.innerText = 'Buzz';
+        } else {
+            mydiv.className = 'yellow';
+            mydiv.innerText = i;
+        }
+
+        mydivs.push(mydiv);
+
+        // 1 row with 4 columns
+        if (i % 4 === 0) {
+            addNewRow(mydivs);
+            // reset mydivs array
+            mydivs = [];
+        }
+    }
+
+    // add remaining rows if any
+    addNewRow(mydivs);
+}
+
+function addNewRow(mydivs) {
+    var mycolumns = [];
+    for (var j = 0; j < mydivs.length; j++) {
+        var mycolumn = document.createElement('div');
+        mycolumn.className = 'col-sm-3'
+
+        // add each div block to column
+        mycolumn.appendChild(mydivs[j]);
+        mycolumns.push(mycolumn);
+    }
+    var myrow = document.createElement('div');
+    myrow.className = 'row';
+
+    for (var k = 0; k < mycolumns.length; k++) {
+        // add each column to row
+        myrow.appendChild(mycolumns[k]);
+    }
+
+    // add row to main
+    mymain.appendChild(myrow);
+}
